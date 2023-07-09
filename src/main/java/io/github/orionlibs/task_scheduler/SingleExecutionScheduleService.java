cb80f723b2588e6e580a0856567fc1668287dd44
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2023-07-09 23:03:22 +0100

    implement task cancellation

--- src/main/java/io/github/orionlibs/task_scheduler/SingleExecutionScheduleService.java
+++ src/main/java/io/github/orionlibs/task_scheduler/SingleExecutionScheduleService.java
@@ -3,17 +3,22 @@ package io.github.orionlibs.task_scheduler;
 import io.github.orionlibs.task_scheduler.config.ConfigurationService;
 import io.github.orionlibs.task_scheduler.config.OrionConfiguration;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Handler;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public class SingleExecutionScheduleService
 {
     private final static Logger log;
     private OrionConfiguration featureConfiguration;
     private ScheduledExecutorService executorService;
+    private Map<ScheduledFuture<?>, Runnable> scheduledTasksToRunnablesMapper;
 
     static
     {
@@ -24,6 +29,7 @@ public class SingleExecutionScheduleService
     {
         setupConfiguration();
         this.executorService = Executors.newSingleThreadScheduledExecutor();
+        this.scheduledTasksToRunnablesMapper = new HashMap<>();
     }
 
 
@@ -46,17 +52,51 @@ public class SingleExecutionScheduleService
     }
 
 
-    public void schedule(Runnable command, long delay, TimeUnit unit) throws FeatureIsDisabledException
+    public ScheduledFuture<?> schedule(Runnable command, long delay, TimeUnit unit) throws FeatureIsDisabledException
     {
         if(ConfigurationService.getBooleanProp("orionlibs.task-scheduler.enabled"))
         {
-            executorService.schedule(command, delay, unit);
+            cleanUpTasks();
+            ScheduledFuture<?> task = executorService.schedule(command, delay, unit);
+            scheduledTasksToRunnablesMapper.put(task, command);
             log.info("schedule started");
+            return task;
         }
         else
         {
             throw new FeatureIsDisabledException();
         }
+    }
+
+
+    public boolean cancelTask(ScheduledFuture<?> taskToCancel) throws FeatureIsDisabledException, TaskDoesNotExistException
+    {
+        if(ConfigurationService.getBooleanProp("orionlibs.task-scheduler.enabled"))
+        {
+            if(scheduledTasksToRunnablesMapper.get(taskToCancel) != null && !taskToCancel.isCancelled())
+            {
+                return taskToCancel.cancel(true);
+            }
+            else
+            {
+                throw new TaskDoesNotExistException();
+            }
+        }
+        else
+        {
+            throw new FeatureIsDisabledException();
+        }
+    }
+
+
+    private void cleanUpTasks()
+    {
+        scheduledTasksToRunnablesMapper.entrySet()
+                        .stream()
+                        .map(task -> task.getKey())
+                        .filter(task -> task.isDone())
+                        .collect(Collectors.toSet())
+                        .forEach(task -> scheduledTasksToRunnablesMapper.remove(task));
     }
 
 
